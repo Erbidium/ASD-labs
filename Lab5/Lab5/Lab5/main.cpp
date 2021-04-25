a887acf26e631fe60ca0858b596e6d397d4c8733
Oleksandr <OlexandrTanasienko@gmail.com>
2021-04-26 02:56:36 +0300

    Change console message

--- Lab5/Lab5/Lab5/main.cpp
+++ Lab5/Lab5/Lab5/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter number: ";
+    cout<<"Enter amount of numbers: ";
     cin>>n;
     vector<int> arr(n, 0);
     for(int i=0;i<n;i++)
